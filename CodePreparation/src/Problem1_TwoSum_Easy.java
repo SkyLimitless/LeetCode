78191e8dfa64cc36391ef0e77e4e706acfae0f63
Aakash Chaurasia <aakash.chaurasia@nextiva.com>
2016-08-30 12:12:41 -0700

    Problem 1 Solution

---
+++ CodePreparation/src/Problem1_TwoSum_Easy.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by achaurasia on 8/30/16.
+ */
+public class Problem1_TwoSum_Easy {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> set = new HashMap<>();
+        Integer first = -1;
+        for(int i = 0; i < nums.length; i++) {
+            first = set.get(target - nums[i]);
+            if(first != null) {
+                return new int[]{first, i};
+            } else {
+                set.put(nums[i], i);
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
