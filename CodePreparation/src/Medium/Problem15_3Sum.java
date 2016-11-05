f26e9343685876eeb8ddb0efc9647e57d56c79bc
aakash-chaurasia <aakash.chaurasia91@gmail.com>
2016-11-05 05:35:13 -0700

    Problem15_3Sum

---
+++ CodePreparation/src/Medium/Problem15_3Sum.java
@@ -0,0 +1,44 @@
+package Medium;
+
+import java.util.*;
+
+/**
+ * Created by Aakash on 11/5/2016.
+ */
+public class Problem15_3Sum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            int first = nums[i];
+            if(first > 0)
+                break;
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int temp = first + nums[j] + nums[k];
+                if(temp == 0) {
+                    List<Integer> l = new ArrayList<>();
+                    l.add(first);
+                    l.add(nums[j]);
+                    l.add(nums[k]);
+                    result.add(l);
+                    while(j < k && nums[j] == nums[k]) j++;
+                    j++;
+                } else if(temp > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+            while(i <  nums.length - 2 && nums[i] == nums[i + 1])i++;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int a[] = {1, 1, -2};
+        Problem15_3Sum problem15_3Sum = new Problem15_3Sum();
+        problem15_3Sum.threeSum(a);
+    }
+}
