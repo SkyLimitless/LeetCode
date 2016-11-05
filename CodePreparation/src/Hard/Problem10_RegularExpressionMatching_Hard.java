5dff1b724d781fcf05a6171d40feb7a91755b924
aakash-chaurasia <aakash.chaurasia91@gmail.com>
2016-11-05 04:15:00 -0700

    Problem 12 Regex Matching

--- CodePreparation/src/Hard/Problem10_RegularExpressionMatching_Hard.java
+++ CodePreparation/src/Hard/Problem10_RegularExpressionMatching_Hard.java
@@ -5,27 +5,37 @@ package Hard;
  */
 public class Problem10_RegularExpressionMatching_Hard {
 
-    public boolean isMatch(String s, String p) {
-        int index = 0;
-        int sLen = s.length();
-        int pLen = p.length();
-        char currChar = p.charAt(index);
-        char prev = '\0';
-        for(int i = 0; i < sLen; i++) {
-            char strChar = s.charAt(i);
-            currChar = p.charAt(index);
-            if(currChar == '.') {
-                index++;
-                prev = '\0';
-            } else if(currChar == '*') {
-
-            } else if(currChar == strChar) {
-                index++;
-                prev = strChar;
-            } else {
-                return false;
+    public boolean isMatch(String str, String pattern) {
+        char[] s = str.toCharArray();
+        char[] p = pattern.toCharArray();
+        boolean[][] rt = new boolean[s.length + 1][p.length + 1];
+        rt[0][0] = true;
+        for (int i = 1; i < rt[0].length; i++) {
+            if(p[i - 1] == '*') {
+                rt[0][i] = rt[0][i - 2];
             }
         }
-        return true;
+
+        for (int i = 1; i < rt.length; i++) {
+            for (int j = 1; j < rt[0].length; j++) {
+                if(p[j - 1] == '.' || p[j - 1] == s[i - 1]) {
+                    rt[i][j] = rt[i - 1][j - 1];
+                } else if (p[j - 1] == '*') {
+                    if (rt[i][j - 2] || ((p[j - 2] == '.' || p[j - 2] == s[i - 1]) && rt[i - 1][j])) {
+                        rt[i][j] = true;
+                    }
+                }
+            }
+        }
+        return rt[s.length][p.length];
+    }
+
+    public static void main(String[] args) {
+        Problem10_RegularExpressionMatching_Hard regularExpressionMatching_hard = new Problem10_RegularExpressionMatching_Hard();
+        if (regularExpressionMatching_hard.isMatch("aakash", "a*.a.hb.")) {
+            System.out.println("Regex matched");
+        } else {
+            System.out.println("Regex not matched");
+        }
     }
 }
