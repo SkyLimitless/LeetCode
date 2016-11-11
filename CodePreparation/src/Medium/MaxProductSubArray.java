d9d5529a2d30bc230917cb413f5348c85deb612e
aakash-chaurasia <aakash.chaurasia91@gmail.com>
2016-11-11 02:24:33 -0700

    Continous Max Product

---
+++ CodePreparation/src/Medium/MaxProductSubArray.java
@@ -0,0 +1,34 @@
+package Medium;
+
+/**
+ * Created by achaurasia on 11/11/16.
+ * Find the contiguous subarray within an array (containing at least one number) which has the largest product.
+ * For example, given the array [2,3,-2,4],
+ * the contiguous subarray [2,3] has the largest product = 6.
+ */
+public class MaxProductSubArray {
+    public static void main(String[] args) {
+        MaxProductSubArray maxProductSubArray = new MaxProductSubArray();
+        int a[] = {-2, 3, -4};
+        int a1[] = {-4, -3, -2};
+        int res = maxProductSubArray.maxProduct(a1);
+        System.out.println("res = " + res);
+    }
+
+    public int maxProduct(int[] nums) {
+        if(nums.length == 0) {
+            return 0;
+        }
+        int maxHere = nums[0];
+        int minHere = nums[0];
+        int maxSoFar = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            int t = nums[i];
+            int maxPre = maxHere;
+            maxHere = Math.max(Math.max(maxHere * t, minHere * t), t);
+            minHere = Math.min(Math.min(maxPre * t, minHere * t), t);
+            maxSoFar = Math.max(maxSoFar, maxHere);
+        }
+        return maxSoFar;
+    }
+}
