7195a315a701af7bc4009a1f6b758efc7aaefa7e
aakash-chaurasia <aakash.chaurasia91@gmail.com>
2016-11-18 17:06:29 -0700

    Problem 23 Completed

--- CodePreparation/src/Easy/Problem21_MergeTwoSortedLists.java
+++ CodePreparation/src/Easy/Problem21_MergeTwoSortedLists.java
@@ -1,5 +1,8 @@
 package Easy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by achaurasia on 11/18/16.
  */
@@ -19,7 +22,8 @@ public class Problem21_MergeTwoSortedLists {
         ListNode head2 = new ListNode(4);
         head2.next = new ListNode(6);
         head2.next.next = new ListNode(9);
-        ListNode result = mergeTwoSortedLists.mergeTwoLists(head1, head2);
+        //ListNode result = mergeTwoSortedLists.mergeTwoLists(head1, head2);
+        ListNode result = mergeTwoSortedLists.mergeKLists(new ListNode[]{head1, head2});
         while(result != null) {
             System.out.println("result.val = " + result.val);
             result = result.next;
@@ -65,5 +69,33 @@ public class Problem21_MergeTwoSortedLists {
             temp.next = l2;
         }
         return result;
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        int len = lists.length;
+        int mid = len/2;
+        if(len == 0) {
+            return null;
+        }
+        if(len == 1) {
+            return lists[0];
+        }
+        ListNode[] a = Arrays.copyOfRange(lists, 0, mid);
+        ListNode[] b = Arrays.copyOfRange(lists, mid, len);
+        ListNode t1 = mergeKLists(a);
+        ListNode t2 = mergeKLists(b);
+        return merge(t1, t2);
+    }
+
+    public ListNode merge(ListNode l1,ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = merge(l1, l2.next);
+            return l2;
+        }
     }
 }
